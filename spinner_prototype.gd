992db173d754ef7da9af597309ae6c3bf5870825
Kim Prower <kmhr@kmhr.dev>
2026-01-02 01:20:27 -0500

    misunderstood 'alpha' in as used in rgb8 color, made nearly invisible triangles instead of fully opaque

--- spinner_prototype.gd
+++ spinner_prototype.gd
@@ -1,20 +1,21 @@
 extends Node2D
 
 # learn to create a wedge class... yikes
-var wedge_count = 7
+var wedge_count : float = 7
 var wedge_center : Array = [0,0]
 
 var wedge_depth = 15
-var wedge_width = 1/wedge_count
+var wedge_width : float = 1 / wedge_count
 
 # for an iscoceles triangle "pointed straight down" 
 # bottom point at origin, [top-left point], [top-right point]
 # this should be a function but I'll figure that out later
 var simple_wedge : Array = [
-	wedge_center, 
-	[wedge_center[0] + cos(0.5 * PI * wedge_width),wedge_center[1] + sin(0.5 * PI * wedge_width)],
-	[-wedge_center[0] + cos(0.5 * PI * wedge_width),wedge_center[1] + sin(0.5 * PI * wedge_width)]
+	[0,0],#wedge_center, #
+	[-wedge_depth * sin(PI * wedge_width),-wedge_depth * cos(PI * wedge_width)], #
+	[wedge_depth * sin(PI * wedge_width),-wedge_depth * cos(PI * wedge_width)] #
 ]
+
 
 # https://docs.godotengine.org/en/stable/tutorials/2d/custom_drawing_in_2d.html#example-1-drawing-a-custom-shape
 var my_wedge : PackedVector2Array
@@ -31,7 +32,7 @@ func _ready():
 	
 func _draw():
 	# We are going to paint with this color.
-	var my_wedge_color : Color = Color.from_rgba8(101, 129, 226, 1)
+	var my_wedge_color : Color = Color.from_rgba8(184, 52, 192, 255)
 
 	# We pass the PackedVector2Array to draw the shape.
 	draw_polygon(my_wedge, [ my_wedge_color ])
@@ -39,7 +40,3 @@ func _draw():
 
 func compute_triangle():
 	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta) -> void:
-	queue_redraw()
